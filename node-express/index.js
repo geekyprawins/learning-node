85a6c541c004fdbf93b409f8946235ab870ff8b7
Praveen Varma <swamiraju.varma@learner.manipal.edu>
2021-09-17 13:54:04 +0530

    express router

--- node-express/index.js
+++ node-express/index.js
@@ -1,3 +1,4 @@
+const bodyParser = require("body-parser");
 const express = require("express");
 const http = require("http");
 const hostname = "localhost";
@@ -5,12 +6,31 @@ const port = 3000;
 const app = express();
 const morgan = require("morgan");
 app.use(morgan("dev"));
+app.use(bodyParser.json());
+const dishRouter = require('./routes/dish-router')
+app.use('/dishes', dishRouter)
+// another
+app.get("/dishes/:dishId", (req, res, next) => {
+  res.end("Sending details of dish: " + req.params.dishId);
+});
+app.post("/dishes/:dishId", (req, res, next) => {
+  res.statusCode = 403;
+  res.end("POST not supported");
+});
+app.put("/dishes/:dishId", (req, res, next) => {
+  res.write("Updating the dish " + req.params.dishId);
+  res.end(
+    "Will update the dish " + req.body.name + "with " + req.body.description
+  );
+});
+app.delete("/dishes/:dishId", (req, res, next) => {
+  res.end("Deleting the dish: " + req.params.dishId);
+});
 
-app.use(express.static(__dirname+'/public'));
-
+app.use(express.static(__dirname + "/public"));
 
 app.use((req, res, next) => {
-//   console.log(req.headers);
+  //   console.log(req.headers);
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/plain");
   res.end("Hello Peter");
