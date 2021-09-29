689d38b7d71c8e91c91656479dd601e819930f96
Praveen Varma <swamiraju.varma@learner.manipal.edu>
2021-09-29 20:41:00 +0530

    cookies

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -32,32 +32,45 @@ app.set("view engine", "jade");
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+// cookies here
+app.use(cookieParser("123-456-789"));
+
 // authentication middleware
 function auth(req, res, next) {
-  console.log("Auth headers: "+req.headers);
-  var authHeader = req.headers.authorization;
-  if (!authHeader) {
-    var err = new Error("Not authenticated");
-    res.setHeader("WWW-Authenticate", "Basic");
-    err.status = 401;
-    return next(err);
-  }
-  var auth = new Buffer.from(authHeader.split(" ")[1], "base64")
-    .toString()
-    .split(":");
-  var userName = auth[0];
-  var password = auth[1];
-  if (userName == "admin" && password == "password") {
-    next();
+  console.log("Auth headers: " + req.headers);
+  console.log("Cookies: " + req.signedCookies);
+  if (!req.signedCookies.user) {
+    var authHeader = req.headers.authorization;
+    if (!authHeader) {
+      var err = new Error("Not authenticated");
+      res.setHeader("WWW-Authenticate", "Basic");
+      err.status = 401;
+      return next(err);
+    }
+    var auth = new Buffer.from(authHeader.split(" ")[1], "base64")
+      .toString()
+      .split(":");
+    var userName = auth[0];
+    var password = auth[1];
+    if (userName == "admin" && password == "password") {
+      res.cookie("user", "admin", { signed: true });
+      next();
+    } else {
+      var err = new Error("Not authenticated");
+      res.setHeader("WWW-Authenticate", "Basic");
+      err.status = 401;
+      return next(err);
+    }
   } else {
-    var err = new Error("Not authenticated");
-    res.setHeader("WWW-Authenticate", "Basic");
-    err.status = 401;
-    return next(err);
+    if (req.signedCookies.user === "admin") {
+      next();
+    } else {
+      var err = new Error("Not authenticated");
+      err.status = 401;
+      return next(err);
+    }
   }
 }
-
 app.use(auth);
 
 app.use(express.static(path.join(__dirname, "public")));
