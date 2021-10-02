1c75ac35876b103857d3c7fb860ab74f73d6d15d
Praveen Varma <swamiraju.varma@learner.manipal.edu>
2021-10-02 21:02:55 +0530

    Sessions

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -3,6 +3,8 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+var session = require("express-session");
+var FileStore = require("session-file-store");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
@@ -33,40 +35,36 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 // cookies here
-app.use(cookieParser("123-456-789"));
+// app.use(cookieParser("123-456-789"));
 
+app.use(
+  session({
+    name: "session-id",
+    secret: "123-456-789",
+    saveUninitialized: false,
+    resave: false,
+    store: new FileStore(),
+  })
+);
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 // authentication middleware
 function auth(req, res, next) {
+  console.log(req.session);
   console.log("Auth headers: " + req.headers);
   console.log("Cookies: " + req.signedCookies);
-  if (!req.signedCookies.user) {
-    var authHeader = req.headers.authorization;
-    if (!authHeader) {
-      var err = new Error("Not authenticated");
-      res.setHeader("WWW-Authenticate", "Basic");
-      err.status = 401;
-      return next(err);
-    }
-    var auth = new Buffer.from(authHeader.split(" ")[1], "base64")
-      .toString()
-      .split(":");
-    var userName = auth[0];
-    var password = auth[1];
-    if (userName == "admin" && password == "password") {
-      res.cookie("user", "admin", { signed: true });
-      next();
-    } else {
-      var err = new Error("Not authenticated");
-      res.setHeader("WWW-Authenticate", "Basic");
-      err.status = 401;
-      return next(err);
-    }
+  if (!req.session.user) {
+    var err = new Error("Not authenticated");
+
+    err.status = 401;
+    return next(err);
   } else {
-    if (req.signedCookies.user === "admin") {
+    if (req.session.user === "authenticated") {
       next();
     } else {
       var err = new Error("Not authenticated");
-      err.status = 401;
+      err.status = 403;
       return next(err);
     }
   }
@@ -75,8 +73,6 @@ app.use(auth);
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
 app.use("/dishes", dishesRouter);
 app.use("/promotions", promotionsRouter);
 app.use("/leaders", leadersRouter);
--- conFusionServer/routes/users.js
+++ conFusionServer/routes/users.js
@@ -1,9 +1,89 @@
-var express = require('express');
+var express = require("express");
+const bodyParser = require("body-parser");
+var User = require("../models/user");
+
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  res.send("respond with a resource");
+});
+
+router.post("/signup", function (req, res, next) {
+  User.findOne({ username: req.body.username })
+    .then((user) => {
+      if (user !== null) {
+        res.status(400).send("Username already exists");
+        next;
+      } else {
+        return User.create({
+          username: req.body.username,
+          password: req.body.password,
+        });
+      }
+    })
+    .then(
+      (user) => {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "application/json");
+        res.json(user);
+      },
+      (err) => {
+        next(err);
+      }
+    );
+});
+
+router.post("/login", (req, res, next) => {
+  if (!req.session.user) {
+    var authHeader = req.headers.authorization;
+    if (!authHeader) {
+      var err = new Error("Not authenticated");
+      res.setHeader("WWW-Authenticate", "Basic");
+      err.status = 401;
+      return next(err);
+    }
+    var auth = new Buffer.from(authHeader.split(" ")[1], "base64")
+      .toString()
+      .split(":");
+    var userName = auth[0];
+    var password = auth[1];
+    User.findOne({ username: userName })
+      .then((user) => {
+        if (user === null) {
+          var err = new Error("User not found");
+          err.status = 403;
+          return next(err);
+        } else if (user.password !== password) {
+          var err = new Error("Wrong password");
+          err.status = 403;
+          return next(err);
+        } else if (user.username === userName && user.password === password) {
+          // res.cookie("user", "admin", { signed: true });
+          req.session.user = "authenticated";
+          res.statusCode = 200;
+          res.setHeader("Content-Type", "application/json");
+          res.json(user);
+        }
+      })
+      .catch((err) => next);
+  } else {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "application/json");
+    res.json(req.session.user);
+  }
+});
+
+router.get("/logout", (req, res, next) => {
+  if (req.session) {
+    req.session.destroy();
+    res.clearCookie("session-id");
+    res.redirect("/");
+  } else {
+    var err = new Error("You are not logged in");
+    err.status = 403;
+    return next(err);
+  }
 });
 
 module.exports = router;
