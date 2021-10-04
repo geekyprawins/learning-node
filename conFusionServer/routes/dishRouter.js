dc5675493e90bfe4beb848f146ac7cf6fe9ffadc
Praveen Varma <swamiraju.varma@learner.manipal.edu>
2021-10-05 00:07:57 +0530

    Mongoose population

--- conFusionServer/routes/dishRouter.js
+++ conFusionServer/routes/dishRouter.js
@@ -15,6 +15,7 @@ dishRouter
   })
   .get((req, res, next) => {
     Dishes.find({})
+      .populate("comments.author")
       .then(
         (dish) => {
           res.statusCode = 200;
@@ -69,6 +70,7 @@ dishRouter
   .route("/:dishId")
   .get((req, res, next) => {
     Dishes.findById(req.params.dishId)
+      .populate("comments.author")
       .then(
         (dish) => {
           console.log("Dish created ", dish);
@@ -136,6 +138,7 @@ dishRouter
   })
   .get((req, res, next) => {
     Dishes.findById(req.params.dishId)
+      .populate("comments.author")
       .then(
         (dish) => {
           if (dish != null) {
@@ -159,8 +162,10 @@ dishRouter
       .then(
         (dish) => {
           if (dish != null) {
+            req.body.author = req.user._id;
             dish.comments.push(req.body);
             dish.save().then((dish) => {
+              Dishes.findById(dish._id).populate("comments.author");
               res.statusCode = 200;
               res.setHeader("Content-Type", "application/json");
               res.json(dish);
@@ -262,9 +267,13 @@ dishRouter
 
             dish.save().then(
               (dish) => {
-                res.statusCode = 200;
-                res.setHeader("Content-Type", "application/json");
-                res.json(dish);
+                Dishes.findById(dish._id)
+                  .populate("comments.author")
+                  .then((dish) => {
+                    res.statusCode = 200;
+                    res.setHeader("Content-Type", "application/json");
+                    res.json(dish);
+                  });
               },
               (err) => next(err)
             );
--- conFusionServer/routes/users.js
+++ conFusionServer/routes/users.js
@@ -19,14 +19,29 @@ router.post("/signup", function (req, res, next) {
         res.setHeader("Content-Type", "application/json");
         res.json({ err: err });
       } else {
-        passport.authenticate("local")(req, res, () => {
-          res.statusCode = 200;
-          res.setHeader("Content-Type", "application/json");
-          res.json({ success: true, status: "Registration Successful!" });
+        if (req.body.firstname) {
+          user.firstname = req.body.firstname;
+        }
+        if (req.body.lastname) {
+          user.lastname = req.body.lastname;
+        }
+        user.save((err, user) => {
+          if (err) {
+            res.statusCode = 400;
+            res.setHeader("Content-Type", "application/json");
+            res.json({ err: err });
+            return;
+          }
+          passport.authenticate("local")(req, res, () => {
+            res.statusCode = 200;
+            res.setHeader("Content-Type", "application/json");
+            res.json({ success: true, status: "Registration Successful!" });
+          });
         });
       }
     }
-  ); // end of User.register
+    // end of User.register
+  );
 });
 
 router.post("/login", passport.authenticate("local"), (req, res) => {
